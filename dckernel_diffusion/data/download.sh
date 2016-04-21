39b28d6e4f712803c8dbed2c9f2d1908bb39536b
Hisashi Yashiro <h.yashiro@riken.jp>
2016-04-22 01:25:07 +0900

    Ignore large data

---
+++ dckernel_diffusion/data/download.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -x
+
+# please get from http://scale.aics.riken.jp/yashiro/download/snapshot.dc_diffusion.pe000000
+
+rm -f ./snapshot.dc_diffusion.pe000000
+wget http://scale.aics.riken.jp/yashiro/download/snapshot.dc_diffusion.pe000000
---
+++ dckernel_divdamp/data/download.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -x
+
+# please get from http://scale.aics.riken.jp/yashiro/download/snapshot.dc_divdamp3d.pe000000
+
+rm -f ./snapshot.dc_divdamp3d.pe000000
+wget http://scale.aics.riken.jp/yashiro/download/snapshot.dc_divdamp3d.pe000000
