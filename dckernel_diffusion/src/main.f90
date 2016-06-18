176706bf04d569f7f9aeecac28fdbd9c60d79055
Hisashi Yashiro <h.yashiro@riken.jp>
2016-06-18 19:12:02 +0200

    Enable to use single precision

--- dckernel_diffusion/src/main.f90
+++ dckernel_diffusion/src/main.f90
@@ -16,13 +16,27 @@ program dckernel_diffusion
   !-----------------------------------------------------------------------------
   implicit none
 
-  real(RP), allocatable :: dscl   (:,:,:)
-  real(RP), allocatable :: dscl_pl(:,:,:)
-  real(RP), allocatable :: scl    (:,:,:)
-  real(RP), allocatable :: scl_pl (:,:,:)
-  real(RP), allocatable :: kh     (:,:,:)
-  real(RP), allocatable :: kh_pl  (:,:,:)
+  real(DP), allocatable :: ORG_dscl          (:,:,:)
+  real(DP), allocatable :: ORG_dscl_pl       (:,:,:)
+  real(DP), allocatable :: ORG_scl           (:,:,:)
+  real(DP), allocatable :: ORG_scl_pl        (:,:,:)
+  real(DP), allocatable :: ORG_kh            (:,:,:)
+  real(DP), allocatable :: ORG_kh_pl         (:,:,:)
+  real(DP), allocatable :: ORG_cinterp_TN    (:,:,:,:)
+  real(DP), allocatable :: ORG_cinterp_TN_pl (:,:,:,:)
+  real(DP), allocatable :: ORG_cinterp_HN    (:,:,:,:)
+  real(DP), allocatable :: ORG_cinterp_HN_pl (:,:,:)
+  real(DP), allocatable :: ORG_cinterp_TRA   (:,:,:)
+  real(DP), allocatable :: ORG_cinterp_TRA_pl(:,:)
+  real(DP), allocatable :: ORG_cinterp_PRA   (:,:)
+  real(DP), allocatable :: ORG_cinterp_PRA_pl(:,:)
 
+  real(RP), allocatable :: dscl          (:,:,:)
+  real(RP), allocatable :: dscl_pl       (:,:,:)
+  real(RP), allocatable :: scl           (:,:,:)
+  real(RP), allocatable :: scl_pl        (:,:,:)
+  real(RP), allocatable :: kh            (:,:,:)
+  real(RP), allocatable :: kh_pl         (:,:,:)
   real(RP), allocatable :: cinterp_TN    (:,:,:,:)
   real(RP), allocatable :: cinterp_TN_pl (:,:,:,:)
   real(RP), allocatable :: cinterp_HN    (:,:,:,:)
@@ -44,13 +58,12 @@ program dckernel_diffusion
 
   write(*,*) "*** Start  initialize"
 
-  allocate( dscl          (ADM_gall   ,ADM_kall,ADM_lall   ) )
-  allocate( dscl_pl       (ADM_gall_pl,ADM_kall,ADM_lall_pl) )
-  allocate( scl           (ADM_gall   ,ADM_kall,ADM_lall   ) )
-  allocate( scl_pl        (ADM_gall_pl,ADM_kall,ADM_lall_pl) )
-  allocate( kh            (ADM_gall   ,ADM_kall,ADM_lall   ) )
-  allocate( kh_pl         (ADM_gall_pl,ADM_kall,ADM_lall_pl) )
-
+  allocate( dscl          (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( dscl_pl       (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
+  allocate( scl           (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( scl_pl        (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
+  allocate( kh            (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( kh_pl         (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
   allocate( cinterp_TN    (ADM_gall   ,ADM_lall   ,AI:AJ,ADM_nxyz) )
   allocate( cinterp_TN_pl (ADM_gall_pl,ADM_lall_pl,2    ,ADM_nxyz) )
   allocate( cinterp_HN    (ADM_gall   ,ADM_lall   ,AI:AJ,ADM_nxyz) )
@@ -66,7 +79,6 @@ program dckernel_diffusion
   scl_pl        (:,:,:)   = 0.0_RP
   kh            (:,:,:)   = 0.0_RP
   kh_pl         (:,:,:)   = 0.0_RP
-
   cinterp_TN    (:,:,:,:) = 0.0_RP
   cinterp_TN_pl (:,:,:,:) = 0.0_RP
   cinterp_HN    (:,:,:,:) = 0.0_RP
@@ -81,6 +93,24 @@ program dckernel_diffusion
   check_dscl    (:,:,:)   = 0.0_RP
   check_dscl_pl (:,:,:)   = 0.0_RP
 
+  !###############################################################################
+
+  !---< read input data >---
+  allocate( ORG_dscl          (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( ORG_dscl_pl       (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
+  allocate( ORG_scl           (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( ORG_scl_pl        (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
+  allocate( ORG_kh            (ADM_gall   ,ADM_kall,ADM_lall   )       )
+  allocate( ORG_kh_pl         (ADM_gall_pl,ADM_kall,ADM_lall_pl)       )
+  allocate( ORG_cinterp_TN    (ADM_gall   ,ADM_lall   ,AI:AJ,ADM_nxyz) )
+  allocate( ORG_cinterp_TN_pl (ADM_gall_pl,ADM_lall_pl,2    ,ADM_nxyz) )
+  allocate( ORG_cinterp_HN    (ADM_gall   ,ADM_lall   ,AI:AJ,ADM_nxyz) )
+  allocate( ORG_cinterp_HN_pl (ADM_gall_pl,ADM_lall_pl,      ADM_nxyz) )
+  allocate( ORG_cinterp_TRA   (ADM_gall   ,ADM_lall   ,TI:TJ         ) )
+  allocate( ORG_cinterp_TRA_pl(ADM_gall_pl,ADM_lall_pl               ) )
+  allocate( ORG_cinterp_PRA   (ADM_gall   ,ADM_lall                  ) )
+  allocate( ORG_cinterp_PRA_pl(ADM_gall_pl,ADM_lall_pl               ) )
+
   call MISC_make_idstr(EX_fname,'snapshot.dc_diffusion','pe',SET_prc_me)
   EX_fid = MISC_get_available_fid()
   open( unit   = EX_fid,         &
@@ -89,21 +119,53 @@ program dckernel_diffusion
         access = 'sequential',   &
         status = 'old'           )
 
-     read(EX_fid) dscl          (:,:,:)
-     read(EX_fid) dscl_pl       (:,:,:)
-     read(EX_fid) scl           (:,:,:)
-     read(EX_fid) scl_pl        (:,:,:)
-     read(EX_fid) kh            (:,:,:)
-     read(EX_fid) kh_pl         (:,:,:)
-     read(EX_fid) cinterp_TN    (:,:,:,:)
-     read(EX_fid) cinterp_TN_pl (:,:,:,:)
-     read(EX_fid) cinterp_HN    (:,:,:,:)
-     read(EX_fid) cinterp_HN_pl (:,:,  :)
-     read(EX_fid) cinterp_TRA   (:,:,:  )
-     read(EX_fid) cinterp_TRA_pl(:,:    )
-     read(EX_fid) cinterp_PRA   (:,:    )
-     read(EX_fid) cinterp_PRA_pl(:,:    )
+     read(EX_fid) ORG_dscl          (:,:,:)
+     read(EX_fid) ORG_dscl_pl       (:,:,:)
+     read(EX_fid) ORG_scl           (:,:,:)
+     read(EX_fid) ORG_scl_pl        (:,:,:)
+     read(EX_fid) ORG_kh            (:,:,:)
+     read(EX_fid) ORG_kh_pl         (:,:,:)
+     read(EX_fid) ORG_cinterp_TN    (:,:,:,:)
+     read(EX_fid) ORG_cinterp_TN_pl (:,:,:,:)
+     read(EX_fid) ORG_cinterp_HN    (:,:,:,:)
+     read(EX_fid) ORG_cinterp_HN_pl (:,:,  :)
+     read(EX_fid) ORG_cinterp_TRA   (:,:,:  )
+     read(EX_fid) ORG_cinterp_TRA_pl(:,:    )
+     read(EX_fid) ORG_cinterp_PRA   (:,:    )
+     read(EX_fid) ORG_cinterp_PRA_pl(:,:    )
   close(EX_fid)
+
+  dscl          (:,:,:)   = real( ORG_dscl          (:,:,:)  , kind=RP )
+  dscl_pl       (:,:,:)   = real( ORG_dscl_pl       (:,:,:)  , kind=RP )
+  scl           (:,:,:)   = real( ORG_scl           (:,:,:)  , kind=RP )
+  scl_pl        (:,:,:)   = real( ORG_scl_pl        (:,:,:)  , kind=RP )
+  kh            (:,:,:)   = real( ORG_kh            (:,:,:)  , kind=RP )
+  kh_pl         (:,:,:)   = real( ORG_kh_pl         (:,:,:)  , kind=RP )
+  cinterp_TN    (:,:,:,:) = real( ORG_cinterp_TN    (:,:,:,:), kind=RP )
+  cinterp_TN_pl (:,:,:,:) = real( ORG_cinterp_TN_pl (:,:,:,:), kind=RP )
+  cinterp_HN    (:,:,:,:) = real( ORG_cinterp_HN    (:,:,:,:), kind=RP )
+  cinterp_HN_pl (:,:,  :) = real( ORG_cinterp_HN_pl (:,:,  :), kind=RP )
+  cinterp_TRA   (:,:,:  ) = real( ORG_cinterp_TRA   (:,:,:  ), kind=RP )
+  cinterp_TRA_pl(:,:    ) = real( ORG_cinterp_TRA_pl(:,:    ), kind=RP )
+  cinterp_PRA   (:,:    ) = real( ORG_cinterp_PRA   (:,:    ), kind=RP )
+  cinterp_PRA_pl(:,:    ) = real( ORG_cinterp_PRA_pl(:,:    ), kind=RP )
+
+  deallocate( ORG_dscl           )
+  deallocate( ORG_dscl_pl        )
+  deallocate( ORG_scl            )
+  deallocate( ORG_scl_pl         )
+  deallocate( ORG_kh             )
+  deallocate( ORG_kh_pl          )
+  deallocate( ORG_cinterp_TN     )
+  deallocate( ORG_cinterp_TN_pl  )
+  deallocate( ORG_cinterp_HN     )
+  deallocate( ORG_cinterp_HN_pl  )
+  deallocate( ORG_cinterp_TRA    )
+  deallocate( ORG_cinterp_TRA_pl )
+  deallocate( ORG_cinterp_PRA    )
+  deallocate( ORG_cinterp_PRA_pl )
+
+  !###############################################################################
 
   !---< operator module setup >---
   call OPRT_diffusion_putcoef( cinterp_TN , cinterp_TN_pl , & ![IN]
